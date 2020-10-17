3bac3ee0964c81b281114c8e4e6a804069393023
KazukiNoto <kazuki.noto.0206@gmail.com>
2020-10-17 08:41:55 +0000

    環境構築

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+using WebApiCsv.Data;
+using WebApiCsv.Models;
+
+namespace Csharp_CSV_Read {
+    class Program {
+        static void Main (string[] args) {
+            using (var reader = new StreamReader (location, Encoding.Default))
+            using (var csv = new CsvReader (reader)) {
+                csv.Configuration.RegisterClassMap<StudentMap> ();
+                var records = csv.GetRecords<Student> ().ToList ();
+                return records;
+            }
+        }
+    }
+}
